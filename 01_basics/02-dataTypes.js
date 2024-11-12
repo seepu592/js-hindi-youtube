9961bb9b6bf8ae6bf475801c2bbe712bae87dc25
seepu592 <61904589+seepu592@users.noreply.github.com>
2024-11-12 02:38:50 +0000

    data types

---
+++ 01_basics/02-dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //Treat all js code as newer version 
+//alert(3+3) we are using nodejs, not browser
+let name = "shivendra";
+let age = "34";
+let isloggedIn = false ;
+let state ;
+
+// number => 2 to power 53 
+//bigint 
+//string => ""
+//boolean => true/false 
+//null => standalone value
+//undefined =>
+//symbol 
+
+
+//object
+
+console.log(typeof state);
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,17 @@
-console.log("shivendra");
+const accountId = 144553 ;
+let accontEmail = "shivendra21@gmail.com";
+var accountPassword = "12345" ;
+accountCity = "Jaipur";
+let accountState;
+
+//accountId = 4 ; Not allowed
+accontEmail = "ss@ss.com";
+accountPassword = "768768";
+accountCity = "Bengaluru";
+
+console.log(accountId);
+/* 
+Prefer not to use var
+because of issue in block scope and funtion scope
+*/
+console.table([accountId,accontEmail,accountPassword,accountCity,accountState]);
